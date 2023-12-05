4597d724988f6567183ed424c35586a393c97b94
parisollie <pjff18@gmail.com>
2023-12-05 01:28:10 -0600

    Autenticacion,buena

--- app/src/main/java/com/pjff/mousywater/Login.kt
+++ app/src/main/java/com/pjff/mousywater/Login.kt
@@ -2,10 +2,215 @@ package com.pjff.mousywater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.text.InputType
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.pjff.mousywater.databinding.ActivityLoginBinding
+
+
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    //Para Firebase-PARA LA PRACTICA, y para el proyecto
+    private lateinit var firebaseAuth: FirebaseAuth
+    //Para las cajas de texto
+    private var email = ""
+    private var contrasenia = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //***********************************************************************************
+        //Para el proyecto y practica
+        firebaseAuth = FirebaseAuth.getInstance()
+        //***********************************************************************************
+
+
+        //Para el proyecto
+        //Cuando el usuario hace login
+        binding.btnLogin.setOnClickListener {
+            if(!validaCampos()) return@setOnClickListener
+
+            binding.progressBar.visibility = View.VISIBLE
+
+            //autenticando al usuario
+            autenticaUsuario(email, contrasenia)
+        }
+
+        binding.btnRegistrarse.setOnClickListener {
+            if(!validaCampos()) return@setOnClickListener
+
+            binding.progressBar.visibility = View.VISIBLE
+
+            //Registrando al usuario en Firebase
+            firebaseAuth.createUserWithEmailAndPassword(email, contrasenia).addOnCompleteListener { authResult->
+                if(authResult.isSuccessful){
+                    //Enviar correo para verificación de email
+                    var user_fb = firebaseAuth.currentUser
+                    user_fb?.sendEmailVerification()?.addOnSuccessListener {
+                        Toast.makeText(this, "El correo de verificación ha sido enviado", Toast.LENGTH_SHORT).show()
+                    }?.addOnFailureListener {
+                        Toast.makeText(this, "No se pudo enviar el correo de verificación", Toast.LENGTH_SHORT).show()
+                    }
+
+                    Toast.makeText(this, "Usuario creado", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("psw", contrasenia)
+                    startActivity(intent)
+                    finish()
+
+
+                }else{
+                    binding.progressBar.visibility = View.GONE
+                    manejaErrores(authResult)
+                }
+            }
+        }
+
+        binding.tvRestablecerPassword.setOnClickListener {
+            val resetMail = EditText(it.context)
+            resetMail.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+
+            val passwordResetDialog = AlertDialog.Builder(it.context)
+                .setTitle("Restablecer contraseña")
+                .setMessage("Ingrese su correo para recibir el enlace para restablecer")
+                .setView(resetMail)
+                .setPositiveButton("Enviar") { _, _ ->
+                    val mail = resetMail.text.toString()
+                    if (mail.isNotEmpty()) {
+                        firebaseAuth.sendPasswordResetEmail(mail).addOnSuccessListener {
+                            Toast.makeText(
+                                this,
+                                "El enlace para restablecer la contraseña ha sido enviado a su correo",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }.addOnFailureListener {
+                            Toast.makeText(
+                                this,
+                                "El enlace no se ha podido enviar: ${it.message}",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show() //it tiene la excepción
+                        }
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Favor de ingresar la dirección de correo",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }.setNegativeButton("Cancelar") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+    }
+
+    //Para la practica o proyecto
+    private fun validaCampos(): Boolean{
+        email = binding.tietEmail.text.toString().trim() //para que quite espacios en blanco
+        contrasenia = binding.tietContrasenia.text.toString().trim()
+
+        if(email.isEmpty()){
+            binding.tietEmail.error = "Se requiere el correo"
+            binding.tietEmail.requestFocus()
+            return false
+        }
+
+        //firebaus necesita 6 para contrasenia
+        if(contrasenia.isEmpty() || contrasenia.length < 6){
+            binding.tietContrasenia.error = "Se requiere una contraseña o la contraseña no tiene por lo menos 6 caracteres"
+            binding.tietContrasenia.requestFocus()
+            return false
+        }
+        return true
+    }
+
+    //funcion para errores de firebase,proyecto y practica
+    private fun manejaErrores(task: Task<AuthResult>){
+        var errorCode = ""
+
+        try{
+            errorCode = (task.exception as FirebaseAuthException).errorCode
+        }catch(e: Exception){
+            e.printStackTrace()
+        }
+
+        when(errorCode){
+            "ERROR_INVALID_EMAIL" -> {
+                Toast.makeText(this, "Error: El correo electrónico no tiene un formato correcto", Toast.LENGTH_SHORT).show()
+                binding.tietEmail.error = "Error: El correo electrónico no tiene un formato correcto"
+                binding.tietEmail.requestFocus()
+            }
+            "ERROR_WRONG_PASSWORD" -> {
+                Toast.makeText(this, "Error: La contraseña no es válida", Toast.LENGTH_SHORT).show()
+                binding.tietContrasenia.error = "La contraseña no es válida"
+                binding.tietContrasenia.requestFocus()
+                binding.tietContrasenia.setText("")
+            }
+            "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> {
+                //An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.
+                Toast.makeText(this, "Error: Una cuenta ya existe con el mismo correo, pero con diferentes datos de ingreso", Toast.LENGTH_SHORT).show()
+            }
+            "ERROR_EMAIL_ALREADY_IN_USE" -> {
+                Toast.makeText(this, "Error: el correo electrónico ya está en uso con otra cuenta.", Toast.LENGTH_LONG).show()
+                binding.tietEmail.error = ("Error: el correo electrónico ya está en uso con otra cuenta.")
+                binding.tietEmail.requestFocus()
+            }
+            "ERROR_USER_TOKEN_EXPIRED" -> {
+                Toast.makeText(this, "Error: La sesión ha expirado. Favor de ingresar nuevamente.", Toast.LENGTH_LONG).show()
+            }
+            "ERROR_USER_NOT_FOUND" -> {
+                Toast.makeText(this, "Error: No existe el usuario con la información proporcionada.", Toast.LENGTH_LONG).show()
+            }
+            "ERROR_WEAK_PASSWORD" -> {
+                Toast.makeText(this, "La contraseña porporcionada es inválida", Toast.LENGTH_LONG).show()
+                binding.tietContrasenia.error = "La contraseña debe de tener por lo menos 6 caracteres"
+                binding.tietContrasenia.requestFocus()
+            }
+            "NO_NETWORK" -> {
+                Toast.makeText(this, "Red no disponible o se interrumpió la conexión", Toast.LENGTH_LONG).show()
+            }
+            else -> {
+                Toast.makeText(this, "Error. No se pudo autenticar exitosamente.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    //Para la practica , le pasamos el usuario y el password de las cajas de texto
+    private fun autenticaUsuario(usr: String, psw: String){
+        firebaseAuth.signInWithEmailAndPassword(usr, psw).addOnCompleteListener { authResult ->
+            if(authResult.isSuccessful){
+                Toast.makeText(this, "Autenticación exitosa", Toast.LENGTH_SHORT).show()
+
+                //Si todo es exitoso pasamos a la siguiente pagina
+
+                val intent = Intent(this, MainActivity::class.java)
+                //Es para pasarle las huellas, no es necsario para proyecto
+                intent.putExtra("psw", psw)
+                startActivity(intent)
+                //Finish para quitar el activity de login
+                finish()
+            }else{
+                binding.progressBar.visibility = View.GONE
+                manejaErrores(authResult)
+            }
+        }
     }
 }
+
--- app/src/main/java/com/pjff/mousywater/MainActivity.kt
+++ app/src/main/java/com/pjff/mousywater/MainActivity.kt
@@ -3,12 +3,71 @@ package com.pjff.mousywater
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pjff.mousywater.databinding.ActivityMainBinding
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private var user: FirebaseUser? = null
+    private var userId: String? = null
+
+    //***********************************************************************************
+    //Para el lector Biometrico
+    private var banderaEmailVerificado = true
+    //private var banderaHuellaActiva = false
+    private var psw = ""
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //Recupero el usuario que ya se autentico
+        user = firebaseAuth?.currentUser
+        userId = user?.uid
+
+        //Me pone en la caja de texto el email y el nombre del usuario
+        binding.tvUsuario.text = user?.email
+
+        //Obteniendo el password desde el activity Login
+        val bundle: Bundle? = intent.extras
+        if(bundle != null) {
+            psw = bundle.getString("psw", "")
+        }
+
+        //revisamos si el email no está verificado
+
+        if(user?.isEmailVerified != true){
+            banderaEmailVerificado = false
+            binding.tvCorreoNoVerificado.visibility = View.VISIBLE
+            binding.btnReenviarVerificacion.visibility = View.VISIBLE
+
+            binding.btnReenviarVerificacion.setOnClickListener {
+                user?.sendEmailVerification()?.addOnSuccessListener {
+                    Toast.makeText(this, "El correo de verificación ha sido enviado", Toast.LENGTH_SHORT).show()
+                }?.addOnFailureListener {
+                    Toast.makeText(this, "Error: El correo de verificación no se ha podido enviar", Toast.LENGTH_SHORT).show()
+                    Log.d("LOGS", "onFailure: ${it.message}")
+                }
+            }
+        }
+
+        //Para cerrar sesion
+        binding.btnCerrarSesion.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
     }
 }
