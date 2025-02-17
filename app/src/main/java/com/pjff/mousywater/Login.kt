ef5ff81bacacb7fd34a58caa5f63c9e8e18be8c7
parisollie <pjff18@gmail.com>
2025-02-16 23:22:49 -0600

    Mejorado

--- app/src/main/java/com/pjff/mousywater/Login.kt
+++ app/src/main/java/com/pjff/mousywater/Login.kt
@@ -14,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
 import com.pjff.mousywater.databinding.ActivityLoginBinding
 
+// para acceder es asi : pjff18@gmail.com, 123456
 
 
 class Login : AppCompatActivity() {
